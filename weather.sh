8d6760e1b0c31f3b94bb9f1dbed9c60239fb5af5
ilkhombek <ilkhombefin@gmail.com>
2025-01-15 06:16:24 +0000

    new script checking weater

---
+++ weather.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+while true; do 
+    echo -n "Please type your city: "
+    read city
+    [ ! $city ] && break
+    city= echo $city |tr '[:upper]' '[:lower]'
+    if [ $city = 'tashkent ' ]; then
+             echo "Tashkent is sunshine"
+        elif [ $city = 'London ' ]; then
+             echo "Lonodon is sunshine"
+        elif [ $city = 'Moscow ' ]; then
+             echo "Moscow is sunshine"
+        elif [ $city = 'Paris' ]; then
+             echo "Paris is sunshine"
+         else
+            echo "Прогноз погоды не ясен... Зайдиет плиз.."
+fi
+do
+echo "See you soon..."
